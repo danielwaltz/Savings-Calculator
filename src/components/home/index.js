b091e106803da9a014b4e1d4481855b23d4a5140
Daniel Waltz <danielbwaltz@gmail.com>
2017-08-05 14:17:34 -0400

    Update calculator

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,27 +1,31 @@
 import { h, Component } from 'preact';
-import round from 'round';
 import currencyFormatter from 'currency-formatter';
 import style from './style.less';
+import Header from '../header';
+import Result from '../result';
 
 export default class Home extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			netPaycheck: 0,
 			grossPaycheck: 0,
+			paycheckTakehome: 0,
+			monthlyTakehome: 0,
 			yearlyTakehome: 0,
-			yearlyProjectionAmount: 0,
+			yearlyProjection: 0,
 
 			yearlyIncome: 40000,
 			paycheckFrequency: 26,
-			taxablePercent: 27.2,
+			taxablePercent: 28.8,
 			monthlyExpenses: 0,
-			yearlyProjection: 5,
+			numberOfYears: 5,
 			currency: 'USD',
 		};
 	}
 
 	componentDidMount() {
-		this.calc();
+		this.calculate();
 	}
 
 	handleChange = event => {
@@ -30,125 +34,130 @@ export default class Home extends Component {
 			() => ({
 				[event.target.name]: event.target.value,
 			}),
-			() => this.calc()
+			() => this.calculate()
 		);
 	};
 
-	makeCurrency = amount => {
-		return currencyFormatter.format(round(amount), {
+	toCurrency = amount => {
+		return currencyFormatter.format(amount.toFixed(2), {
 			code: this.state.currency,
 		});
 	};
 
-	calc = () => {
-		const grossPaycheck =
-			this.state.yearlyIncome /
-			this.state.paycheckFrequency /
-			100 *
-			(100 - this.state.taxablePercent);
-		const yearlyTakehome =
-			grossPaycheck * this.state.paycheckFrequency -
-			this.state.monthlyExpenses * 12;
-		const yearlyProjectionAmount = yearlyTakehome * this.state.yearlyProjection;
+	calculate = () => {
+		const netPaycheck = this.state.yearlyIncome / this.state.paycheckFrequency;
+		const grossPaycheck = netPaycheck / 100 * (100 - this.state.taxablePercent);
+		const paycheckTakehome =
+			grossPaycheck -
+			this.state.monthlyExpenses * 12 / this.state.paycheckFrequency;
+		const monthlyTakehome =
+			paycheckTakehome * this.state.paycheckFrequency / 12;
+		const yearlyTakehome = monthlyTakehome * 12;
+		const yearlyProjection = yearlyTakehome * this.state.numberOfYears;
 		this.setState(() => ({
-			grossPaycheck: this.makeCurrency(grossPaycheck),
-			yearlyTakehome: this.makeCurrency(yearlyTakehome),
-			yearlyProjectionAmount: this.makeCurrency(yearlyProjectionAmount),
+			netPaycheck: this.toCurrency(netPaycheck),
+			grossPaycheck: this.toCurrency(grossPaycheck),
+			paycheckTakehome: this.toCurrency(paycheckTakehome),
+			monthlyTakehome: this.toCurrency(monthlyTakehome),
+			yearlyTakehome: this.toCurrency(yearlyTakehome),
+			yearlyProjection: this.toCurrency(yearlyProjection),
 		}));
 	};
 
 	render() {
 		return (
 			<div class={style.home}>
-				<div class={style.results}>
-					<h1>Savings Projection</h1>
-					<p>
-						Gross paycheck:{' '}
-						<strong class={style.resultAmount}>
-							{this.state.grossPaycheck}
-						</strong>
-					</p>
-					<p>
-						Yearly take home:{' '}
-						<strong class={style.resultAmount}>
-							{this.state.yearlyTakehome}
-						</strong>
-					</p>
-					<p>
-						Yearly projection:{' '}
-						<strong class={style.resultAmount}>
-							{this.state.yearlyProjectionAmount}
-						</strong>
-					</p>
+				<Header title="Savings Projection" />
+				<div class={style.calculateWrap}>
+					<div class={style.resultsWrap}>
+						<Result title="Net paycheck" amount={this.state.netPaycheck} />
+						<Result title="Gross paycheck" amount={this.state.grossPaycheck} />
+						<Result
+							title="After expenses"
+							amount={this.state.paycheckTakehome}
+						/>
+						<Result
+							title="Monthly take home"
+							amount={this.state.monthlyTakehome}
+						/>
+						<Result
+							title="Yearly take home"
+							amount={this.state.yearlyTakehome}
+						/>
+						<Result
+							title="Yearly projection"
+							amount={this.state.yearlyProjection}
+						/>
+					</div>
+					<form class={style.formWrap} onSubmit={this.handleChange}>
+						<div class={style.inputWrap}>
+							<label for="yearlyIncome">Net Yearly Income</label>
+							<input
+								type="number"
+								id="yearlyIncome"
+								name="yearlyIncome"
+								value={this.state.yearlyIncome}
+								onkeyup={this.handleChange}
+							/>
+						</div>
+						<div class={style.inputWrap}>
+							<label for="paycheckFrequency">Paycheck Frequency</label>
+							<select
+								id="paycheckFrequency"
+								name="paycheckFrequency"
+								value={this.state.paycheckFrequency}
+								onchange={this.handleChange}
+							>
+								<option value="52">Every Week</option>
+								<option value="26">Every Two Weeks</option>
+								<option value="24">Twice a Month</option>
+							</select>
+						</div>
+						<div class={style.inputWrap}>
+							<label for="taxablePercent">Taxable Percent</label>
+							<input
+								type="text"
+								id="taxablePercent"
+								name="taxablePercent"
+								value={this.state.taxablePercent}
+								onkeyup={this.handleChange}
+							/>
+						</div>
+						<div class={style.inputWrap}>
+							<label for="monthlyExpenses">Monthly Expenses</label>
+							<input
+								type="number"
+								id="monthlyExpenses"
+								name="monthlyExpenses"
+								value={this.state.monthlyExpenses}
+								onkeyup={this.handleChange}
+							/>
+						</div>
+						<div class={style.inputWrap}>
+							<label for="numberOfYears">Number of Years</label>
+							<input
+								type="number"
+								id="numberOfYears"
+								name="numberOfYears"
+								value={this.state.numberOfYears}
+								onkeyup={this.handleChange}
+							/>
+						</div>
+						<div class={style.inputWrap}>
+							<label for="currency">Currency</label>
+							<select
+								id="currency"
+								name="currency"
+								value={this.state.currency}
+								onchange={this.handleChange}
+							>
+								<option value="USD">USD</option>
+								<option value="GBP">GBP</option>
+								<option value="EUR">EUR</option>
+							</select>
+						</div>
+					</form>
 				</div>
-				<form class={style.formWrap} onSubmit={this.handleChange}>
-					<div class={style.inputWrap}>
-						<label for="yearlyIncome">Net Yearly Income</label>
-						<input
-							type="number"
-							id="yearlyIncome"
-							name="yearlyIncome"
-							value={this.state.yearlyIncome}
-							onkeyup={this.handleChange}
-						/>
-					</div>
-					<div class={style.inputWrap}>
-						<label for="paycheckFrequency">Paycheck Frequency</label>
-						<select
-							id="paycheckFrequency"
-							name="paycheckFrequency"
-							value={this.state.paycheckFrequency}
-							onchange={this.handleChange}
-						>
-							<option value="52">Every Week</option>
-							<option value="26">Every Two Weeks</option>
-							<option value="24">Twice a Month</option>
-						</select>
-					</div>
-					<div class={style.inputWrap}>
-						<label for="taxablePercent">Taxable Percent</label>
-						<input
-							type="text"
-							id="taxablePercent"
-							name="taxablePercent"
-							value={this.state.taxablePercent}
-							onkeyup={this.handleChange}
-						/>
-					</div>
-					<div class={style.inputWrap}>
-						<label for="monthlyExpenses">Monthly Expenses</label>
-						<input
-							type="number"
-							id="monthlyExpenses"
-							name="monthlyExpenses"
-							value={this.state.monthlyExpenses}
-							onkeyup={this.handleChange}
-						/>
-					</div>
-					<div class={style.inputWrap}>
-						<label for="yearlyProjection">Number of Years</label>
-						<input
-							type="number"
-							id="yearlyProjection"
-							name="yearlyProjection"
-							value={this.state.yearlyProjection}
-							onkeyup={this.handleChange}
-						/>
-					</div>
-					<div class={style.inputWrap}>
-						<label for="currency">Currency</label>
-						<select
-							id="currency"
-							name="currency"
-							value={this.state.currency}
-							onchange={this.handleChange}
-						>
-							<option value="USD">USD</option>
-							<option value="GBP">GBP</option>
-							<option value="EUR">EUR</option>
-						</select>
-					</div>
-				</form>
 			</div>
 		);
 	}
